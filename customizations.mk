b761dcb9c2cd5d1413829942120141f36734eb3a
SamarV-121 <samarvispute121@pm.me>
2023-04-23 17:07:35 +0530

    overlays: Add brightness slider styles [3/3]
    Change-Id: I5ec57a37727a1d543bc102b84f1d8cfc2d85af3e

--- customizations.mk
+++ customizations.mk
@@ -12,6 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Brightness slider styles
+PRODUCT_PACKAGES += \
+    BrightnessSliderFilledOverlay \
+    BrightnessSliderThinOverlay
+
 # Fonts
 PRODUCT_PACKAGES += \
     FontGoogleSansOverlay \
